b1443466c3c76adf5c53dbd5a9fc7ad0b829de8c
Nikita <n.shuminskiy@richbrains.net>
2022-07-25 20:19:01 +0300

    FEAT/page-404-5

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { NotFound } from '../view/pages/NotFound';
+import { NotFound } from '../view/pages/NotFound/NotFound';
 import { Home } from '../view/pages/Home/Home';
 
 export const Router = (): React.ReactElement => {
