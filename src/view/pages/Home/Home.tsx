861dceedba85e9453bc5fb286bef0e5a1993eead
philipp-moriss <filverem@gmail.com>
2022-07-27 10:54:11 +0300

    fix bugs

--- src/view/pages/Home/Home.tsx
+++ src/view/pages/Home/Home.tsx
@@ -9,6 +9,7 @@ import iconMoney from '../../../assets/icons/mone.svg';
 import { Info } from '../../components/Info/Info';
 import { Title } from '../../components/Title/Title';
 import { Card } from '../../components/Card/Card';
+import { Arrow } from '../../components/Arrow/Arrow';
 
 export const Home = (): React.ReactElement => {
 	return (
@@ -38,25 +39,8 @@ export const Home = (): React.ReactElement => {
 								description={'Download the app for free, sign up and verify your account.'}
 								color={'primary'}
 							/>
-							<svg
-								width="40"
-								height="40"
-								viewBox="0 0 40 40"
-								fill="none"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									d="M20 40C31.0457 40 40 31.0457 40 20C40 8.9543 31.0457 0 20 0C8.9543 0 0 8.9543 0 20C0 31.0457 8.9543 40 20 40Z"
-									fill="#582EFF"
-								/>
-								<path
-									d="M16.1328 12L24.1328 20L16.1328 28"
-									stroke="white"
-									stroke-width="4"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-								/>
-							</svg>
+
+							<Arrow type={'withCircle'} />
 
 							<Card
 								icon={iconNote}
@@ -66,25 +50,8 @@ export const Home = (): React.ReactElement => {
 								}
 								color={'orange'}
 							/>
-							<svg
-								width="40"
-								height="40"
-								viewBox="0 0 40 40"
-								fill="none"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									d="M20 40C31.0457 40 40 31.0457 40 20C40 8.9543 31.0457 0 20 0C8.9543 0 0 8.9543 0 20C0 31.0457 8.9543 40 20 40Z"
-									fill="#582EFF"
-								/>
-								<path
-									d="M16.1328 12L24.1328 20L16.1328 28"
-									stroke="white"
-									stroke-width="4"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-								/>
-							</svg>
+
+							<Arrow type={'withCircle'} />
 
 							<Card
 								icon={iconMoney}
