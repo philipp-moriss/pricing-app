07c23dc5363730b7cefabc31d8d490b40532c794
Nikita <n.shuminskiy@richbrains.net>
2022-07-31 16:09:44 +0300

    create autorization Front-12

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { NotFound } from '../view/pages/NotFound/NotFound';
+import { ForgotPassword } from '../../src/view/pages/ForgotPassword/ForgotPassword';
+import { Login } from '../../src/view/pages/Login/Login';
 import { Home } from '../view/pages/Home/Home';
+import { NotFound } from '../view/pages/NotFound/NotFound';
 
 export const Router = (): React.ReactElement => {
 	return (
 		<Routes>
 			<Route path={'/'} element={<Home />} />
 			<Route path="*" element={<NotFound />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/forgot-password" element={<ForgotPassword />} />
 		</Routes>
 	);
 };
