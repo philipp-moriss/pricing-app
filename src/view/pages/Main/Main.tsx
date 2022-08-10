cd61c83c489f9ae96f3a52446b11b3aec6347bd1
Nikita <n.shuminskiy@richbrains.net>
2022-08-10 16:45:43 +0300

    FEAT/ create universal modal-31

--- src/view/pages/Main/Main.tsx
+++ src/view/pages/Main/Main.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ModalWrapper } from '../../components/Modals/modal-wrapper';
 import { SideBar } from '../../components/SIdeBar/side-bar';
 import styles from './Main.module.scss';
 
 export const Main = (): React.ReactElement => {
+	const [show, setShow] = useState(true);
 	return (
 		<div className={styles['main']}>
 			<div className={styles['main_container']}>
@@ -10,6 +12,20 @@ export const Main = (): React.ReactElement => {
 					<SideBar />
 				</div>
 			</div>
+			{show && (
+				<ModalWrapper
+					closeCallback={() => setShow(false)}
+					callBack={() => setShow(false)}
+				>
+					<h1>Hello</h1>
+					<div>13141414141</div>
+					<div>13141414141</div>
+					<div>13141414141</div>
+					<div>
+						1314141314141414113 14141414113141 41414113141414141 13141414141131414 14141131414141411314 141414113141414141 1314141414114141
+					</div>
+				</ModalWrapper>
+			)}
 		</div>
 	);
 };
