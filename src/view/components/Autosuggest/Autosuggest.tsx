9b779729dc31b8f87dfb7e69efecf7a61cabe022
Nikita <n.shuminskiy@richbrains.net>
2022-08-12 14:49:24 +0300

    FEAT/create_drop_down-34

---
+++ src/view/components/Autosuggest/Autosuggest.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Select from 'react-select';
+import styles from './Autosuggest.module.scss';
+
+const options = [
+	{ value: 'Belarus', label: ' BY' },
+	{ value: 'US', label: ' Dollar' },
+	{ value: 'RU', label: ' RUB' },
+] as any;
+
+export const Autosuggest = () => {
+	const [value, setValue] = useState<string | null>('');
+	return (
+		<div>
+			<Select
+				id={'131'}
+				options={options}
+				className={styles['autosuggest']}
+				classNamePrefix={'autosuggest'}
+				placeholder={'Start typing...'}
+				isDisabled={false}
+				value={value}
+				onChange={(value) => setValue(value)}
+			/>
+		</div>
+	);
+};
