4a1921bd9d3e8ffcf932f80777ea7e8ca4ff02b0
Nikita <n.shuminskiy@richbrains.net>
2022-08-16 23:03:57 +0300

    fix animation for children route Main

--- src/view/pages/Main/Main.tsx
+++ src/view/pages/Main/Main.tsx
@@ -1,33 +1,1 @@
-import React from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { SideBar } from 'view/components/SIdeBar/side-bar';
-
-import MainHeader from '../../components/MainHeader/main-header';
-import styles from './Main.module.scss';
-
-export const Main = (): React.ReactElement => {
-	const location = useLocation();
-	return (
-		<>
-			<MainHeader />
-			<SideBar />
-			<div className={styles['main']}>
-				<div className={styles['main_content']}>
-					<TransitionGroup>
-						<CSSTransition
-							timeout={{ enter: 750, exit: 150 }}
-							key={location.pathname}
-							classNames={'fade'}
-							unmountOnExit
-						>
-							<div className={styles['main_content__page']}>
-								<Outlet />
-							</div>
-						</CSSTransition>
-					</TransitionGroup>
-				</div>
-			</div>
-		</>
-	);
-};
+import React from 'react';import { Outlet, useLocation } from 'react-router-dom';import { CSSTransition, TransitionGroup } from 'react-transition-group';import { SideBar } from 'view/components/SIdeBar/side-bar';import MainHeader from '../../components/MainHeader/main-header';import styles from './Main.module.scss';export const Main = (): React.ReactElement => {	const location = useLocation();	return (		<>			<MainHeader />			<SideBar />			<div className={styles['main']}>				<div className={styles['main_content']}>					<TransitionGroup>						<CSSTransition							timeout={{ enter: 750, exit: 150 }}							key={location.pathname}							classNames={'fade'}							unmountOnExit						>							<div className={styles['main_content__page']}>								<Outlet />							</div>						</CSSTransition>					</TransitionGroup>				</div>			</div>		</>	);};
