fcba9e66cbae841cd1ea1f303756d0d5d13fbdcd
Nikita <n.shuminskiy@richbrains.net>
2022-08-26 13:02:08 +0300

    feat adaptive menu from main, refactoring

---
+++ src/view/components/MobileBar/MobileBar.tsx
@@ -0,0 +1,57 @@
+import { ReactComponent as BagIcon } from 'assets/icons/bag.svg';
+import { ReactComponent as ChartIcon } from 'assets/icons/chart.svg';
+import { ReactComponent as LogoutIcon } from 'assets/icons/log-out.svg';
+import { ReactComponent as PersonIcon } from 'assets/icons/person.svg';
+import { ReactComponent as SettingIcon } from 'assets/icons/setting.svg';
+import { ReactComponent as WalletIcon } from 'assets/icons/wallet.svg';
+import logo from 'assets/logo/logo-pony-web.svg';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import AuthStore from 'store/AuthStore';
+import { useCustomNavigate } from 'utils/hooks/useCustomNav';
+
+import styles from './MobileBar.module.scss';
+
+export const MobileBar = (): React.ReactElement => {
+	const { goTo } = useCustomNavigate();
+	const { t } = useTranslation();
+	const navigateHandler = (to: string): void => {
+		goTo(to);
+		return;
+	};
+	return (
+		<div className={`${styles['mobile-bar']}`}>
+			<div className={styles['mobile-bar_container']}>
+				<div className={styles['mobile-bar_ico-container']}>
+					<WalletIcon
+						onClick={(): void => navigateHandler('/')}
+						className={styles['mobile-bar_ico']}
+					/>
+
+					<BagIcon
+						onClick={(): void => navigateHandler('/work-space')}
+						className={styles['mobile-bar_ico']}
+					/>
+
+					<ChartIcon className={styles['mobile-bar_ico']} />
+
+					<PersonIcon
+						onClick={(): void => navigateHandler('/personal-cabinet')}
+						className={styles['mobile-bar_ico']}
+					/>
+
+					<SettingIcon
+						onClick={(): void => navigateHandler('/settings')}
+						className={styles['mobile-bar_ico']}
+					/>
+					<LogoutIcon
+						onClick={(): void => {
+							AuthStore.removeAuth();
+							goTo('/');
+						}}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
--- src/view/components/UiComponent/Select/Select.tsx
+++ src/view/components/UiComponent/Select/Select.tsx
@@ -5,12 +5,11 @@ export interface SelectProps
 	extends React.DetailedHTMLProps<React.SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>,
 		React.AriaAttributes {
 	options: string[];
-	className: string;
 }
 
-export const Select = ({ options, className, ...rest }: SelectProps): React.ReactElement => {
+export const Select = ({ options, ...rest }: SelectProps): React.ReactElement => {
 	return (
-		<select className={`${styles['select']}`} {...rest}>
+		<select {...rest}>
 			<option></option>;
 			{options.map((option, index) => {
 				return <option key={`${option}-${index}`}>{option}</option>;
