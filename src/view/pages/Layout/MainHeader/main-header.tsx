8d790faf3c1e1ecd6bb112d91c73a201b644c0af
philipp-moriss <p.veremeichik@richbrains.net>
2022-10-14 13:53:26 +0300

    fix styles

--- src/view/pages/Layout/MainHeader/main-header.tsx
+++ src/view/pages/Layout/MainHeader/main-header.tsx
@@ -1,8 +1,10 @@
-import ava from 'assets/images/itachi.png';
+import Avatar from '@mui/material/Avatar';
+import ava from 'assets/images/avatar.png';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import AuthStore from 'store/AuthStore';
 
+import { useResizeWindow } from '../../../../utils/hooks/useCustomNav';
 import styles from './main-header.module.scss';
 
 interface MainHeaderProps {
@@ -12,6 +14,8 @@ interface MainHeaderProps {
 
 const MainHeader = observer(({ toggleSideBar, setToggleSideBar }: MainHeaderProps) => {
 	const { user } = AuthStore;
+	const size = useResizeWindow();
+
 	return (
 		<div className={styles['main-header']}>
 			<button
@@ -25,8 +29,8 @@ const MainHeader = observer(({ toggleSideBar, setToggleSideBar }: MainHeaderProp
 				/>
 			</button>
 			<div className={styles['main-header_avatar-block']}>
-				<img src={ava} alt={'avatar'} />
-				<span>{user ? user.email : 'No name'}</span>
+				<Avatar alt="avatar" src={ava} sx={{ width: 44, height: 44 }} />
+				{size.width > 900 && <span>{user ? user.email : 'No name'}</span>}
 			</div>
 		</div>
 	);
